a535755f5a4ad3cf3c2c1590b88b94eead9b3cdf
ambroiselebs <amberenger06@gmail.com>
2024-11-07 02:04:52 +0100

    better comment on main.c

--- main.c
+++ main.c
@@ -20,11 +20,18 @@ int	main(void)
 	t_handle_touch_params	init_window_touch_params;
 	t_exit_game_params		exit_game_params;
 
+	/* Init window */
 	init_window(&game);
+
+	/* Handle windows related keys */
 	init_window_touch_params.game = &game;
 	mlx_key_hook(game.mlx_win, handle_window_touch, &init_window_touch_params);
+
+	/* Handle windows exit procedure (using button) */
 	exit_game_params.game = &game;
 	mlx_hook(game.mlx_win, 17, 1L << 17, exit_game, &exit_game_params);
+
+	/* Main Loop */
 	mlx_loop(game.mlx);
 	return (0);
 }
